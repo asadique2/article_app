e41a68d157065d8e9a50dae15580ac13468edf6e
a_sadique <asadique@vivifyhealthcare.com>
2025-03-19 16:02:35 +0530

    setup base repository, controller and route

---
+++ article_app/lib/app/routes/app_routes.dart
@@ -0,0 +1,9 @@
+class Routes {
+  static const article = '/article';
+  static const article_details = '/article/details';
+  static const create_article = '/create/article';
+}
+
+class AppPages {
+  static final pages = [];
+}
